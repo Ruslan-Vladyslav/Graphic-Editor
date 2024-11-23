c36c74e5783b32ad5ca7d0524e08dcafeba88805
Ruslan-Vladyslav <rgritsai95@gmail.com>
2024-11-23 12:15:16 +0200

    Add two files

---
+++ WindowsFormsApp1/Interfaces/EllipseInterface.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Drawing;
+
+
+namespace Lab5
+{
+    public interface IEllipseDraw
+    {
+        void DrawEllipse(Graphics g, Pen pen, bool isSolid);
+        void SetEllipse(long x1, long y1, long x2, long y2);
+    }
+}
---
+++ WindowsFormsApp1/Interfaces/LineInterface.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Drawing;
+
+
+namespace Lab5
+{
+    public interface ILineDraw
+    {
+        void DrawLine(Graphics g, Pen pen, bool isSolid);
+        void SetLine(long x1, long y1, long x2, long y2);
+    }
+}
+
